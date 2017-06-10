ff6a2507f6442970eb7327cdd7915f7cb37fc17f
seortiz <v-seortiz@expedia.com>
2017-06-10 14:46:07 -0500

    api call

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,23 @@
-console.log('hacke');
-// console.log(require);
-console.log('simon');
+// var chat
+var $qInput = $("#user-input-q");
+var $qClear = $("#clear-q");
+// var $;
 
-$.ajax('192.168.100.74:3000',[
-  
-])
+function sendMessage(queryMessage) {
+  $.ajax('http://192.168.100.74:3000', {
+    cache: false,
+    dataType: 'json',
+    data: {
+        q : queryMessage
+    },
+    error: function() {
+      console.log('error retrieving data');
+    },
+    success: function(response) {
+      return response
+    },
+    timeout: 5000
+  });
+}
+console.log(sendMessage('hola!'));
+// Initialize liseners
