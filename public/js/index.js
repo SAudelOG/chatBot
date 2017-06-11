a5e888fcb00f638fbff9b9ba2f06dddbcd0401c5
seortiz <v-seortiz@expedia.com>
2017-06-10 20:16:46 -0500

    clean up, add support for show fake data on timeout

--- public/js/index.js
+++ public/js/index.js
@@ -6,23 +6,32 @@ var BotWindow = function($chatWindow) {
   self.$qInput = self.$chatWindow.find('#user-input-q');
   self.$qClear = self.$chatWindow.find('#clear-q');
   self.$conversationBubble = self.$chatWindow.find(".conversation-body");
-  self.API_END_POINT = 'http://192.168.100.74:3000';
-
 
   // Method to send message to the API
   self.sendMessage = function (queryMessage, cb) {
-    $.ajax(self.API_END_POINT, {
+    var API_END_POINT = 'http://192.168.100.74:3000';
+
+    $.ajax(API_END_POINT, {
       cache: false,
       dataType: 'json',
       data: {
           q : queryMessage
       },
-      error: function(e) {
-        cb(e);
+      error: function(request, status, err) {
+        var fakeResponse = {
+          chat: "this is a fake response just for testing"
+        }
+        if (status == "timeout") {
+          // send fake data
+          cb(undefined, fakeResponse);
+        } else {
+          cb(err);
+        }
       },
       success: function(response) {
         cb(undefined, response);
       },
+      timeout: 5000,
     });
   }
 
@@ -32,10 +41,44 @@ var BotWindow = function($chatWindow) {
     self.$conversationBubble.scrollTop(self.$conversationBubble.prop("scrollHeight"));
   };
 
+  self.renderHotels = function(type, hotels) {
+    hotels = [0,1]; // Hack: while we get the real data from the hotels
+    $.each(hotels, function(idx, hotel) {
+      var dataFake = {
+        img: 'http://cdn.luxedb.com/wp-content/uploads/2013/06/Wonderful-Katikies-Hotel-in-Oia-Santorini-Greece-4.jpg',
+        hotelName: 'Katikies Hotel in Oia',
+        rating: '9.3',
+      }
+      // FIXME: this should be a template instead of just be hardcoded
+      self.$conversationBubble.append(
+        '<div class="card-container">' +
+          '<div class="card">' +
+            '<figure class="card-thumbnail">' +
+              '<img src="'+ dataFake.img +'" alt="hotel img">' +
+              '<figcaption class="hotel-name">' +
+                dataFake.hotelName +
+              '</figcaption>' +
+              '<span class="hotel-rating">'+ dataFake.rating +'</span>' +
+            '</figure>' +
+              '<span class="hotel-class">' +
+                '<i class="fa fa-star"></i>' +
+                '<i class="fa fa-star"></i>' +
+                '<i class="fa fa-star"></i>' +
+                '<i class="fa fa-star"></i>' +
+                '<i class="fa fa-star"></i>' +
+              '</span>' +
+              '<button type="button" name="book" class="btn-book">Book now for $40.00</button>' +
+            '</div>' +
+          '</div>'
+      );
+    });
+  };
+
   // Initialize Listeners
 
-  self.$qInput.on('click', function(e) {
-    // console.log('click');
+  self.$qClear.on('click', function(e) {
+    // Clear input
+    self.$qInput.val('');
   });
 
   self.$qInput.keydown(function (e) {
@@ -53,6 +96,11 @@ var BotWindow = function($chatWindow) {
         if (err) throw new Error(err);
         // Send response message to the UI
         self.renderMessage('bot', response.chat);
+        // Send the hotels message to the UI
+        self.renderHotels('bot', response.hotels); // remove this comment and uncomment the sentence
+        // if (response.hotels && response.hotels.length) {
+        //   self.renderHotels('bot', response.hotels);
+        // }
       });
     }
   });
